474fbb1a2264a4ddfa0f65f2b39785e175aa25d2
GabrielDuarteWBJ <gabriel.duarte@webjump.com.br>
2022-12-27 19:06:04 -0300

    fix: fix bug router not working after building

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { AuthProvider } from "./context/auth";
 import { Home } from "./pages/Home";
@@ -7,14 +7,14 @@ import { Login } from "./pages/Login";
 
 const Router = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <AuthProvider>
         <Routes>
           <Route index element={<Home />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </AuthProvider>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
