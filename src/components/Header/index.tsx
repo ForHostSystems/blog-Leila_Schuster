31a3d627e0663f9da2e4108a279ccde0a8edddb9
GabrielDuarteWBJ <gdbiel773@gmail.com>
2023-01-18 13:14:32 -0300

    feature-15 Exibir o Header em todas as telas

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Box } from "@chakra-ui/react";
+
+import { Navegation } from "../Navegation";
+
+export const Header = () => {
+  return (
+    <Box w="100%" h="5rem">
+      <Navegation />
+    </Box>
+  );
+};
--- src/pages/Biography/index.tsx
+++ src/pages/Biography/index.tsx
@@ -5,11 +5,13 @@ import { Divider } from "@chakra-ui/react";
 import { BiographyContent } from "../../components/BiographyContent";
 import { BiographyHeader } from "../../components/BiographyHeader";
 import { Footer } from "../../components/Footer";
+import { Header } from "../../components/Header";
 import { Layout } from "../../components/Layout";
 
 export const Biography = () => {
   return (
     <Layout>
+      <Header />
       <BiographyHeader />
       <Divider w="83%" h="3px" bg="black" mt={10} />
       <BiographyContent />
